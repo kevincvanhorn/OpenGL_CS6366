58887a5473bd72f9bce9f75bfa6d89e578f2bee9
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2019-02-21 18:03:37 -0600

    Added - full rotation function.

--- Asg01_Source/code/Camera.h
+++ Asg01_Source/code/Camera.h
@@ -41,17 +41,11 @@ public:
 	}
 
 	void UpdateCoordinates() {
-		// Calculate the rotated forward vector:
-		//glm::vec3 axisZ;
-		//axisZ.x = cos(glm::radians(RotY)) * cos(glm::radians(RotX));
-		//axisZ.y = sin(glm::radians(RotX));
-		//axisZ.z = sin(glm::radians(RotY)) * cos(glm::radians(RotX));
-		//axisZ = glm::normalize(axisZ);
 	}
 
 	// Called after yaw/pitch/roll values have changed.
 	void OnUpdateRotation() {
-		UpdateCoordinates();
+	
 	}
 
 	glm::mat4 GetMVPMatrix()
@@ -76,35 +70,14 @@ public:
 		tempUp = tempUp * rot;
 		axisY = glm::vec3(tempUp.x, tempUp.y, tempUp.z);
 
-		/*glm::mat4 rot = glm::mat4(1.0f);
-		glm::vec4 rVec = glm::vec4(initialLoc.x, initialLoc.y, initialLoc.z, 1);
-		rot = glm::rotate(rot, glm::radians(RotY), glm::vec3(0,1,0));
-		rVec = rVec* rot;
-		axisX = glm::vec3(rVec.x, rVec.y, rVec.z);*/
-
-		// Rotate Front/Roll/Z):
-		/*glm::mat4 rollRot = glm::mat4(1.0f);
-		glm::vec4 tAxisY = glm::vec4(axisY.x, axisY.y, axisY.z, 1);
-		glm::vec4 tLoc = glm::vec4(initialLoc.x, initialLoc.y, initialLoc.z, 1);
-		rollRot = glm::rotate(rollRot, glm::radians(RotZ), glm::vec3(1, 0, 0));
-		tAxisY = tAxisY * rollRot;
-		tLoc = tLoc * rollRot;
-		axisY = glm::vec3(tAxisY.x, tAxisY.y, tAxisY.z);
-		axisZ = glm::vec3(tLoc.x, tLoc.y, tLoc.z);*/
-
-
-		//Rotate All
-		/*glm::mat4 rot = glm::mat4(1.0f);
-		glm::vec4 tPoint = glm::vec4(initialLoc.x, initialLoc.y, initialLoc.z, 1);
-		glm::vec4 tAxisY = glm::vec4(axisY.x, axisY.y, axisY.z, 1);
-
+		// Rotate along Right
+		//rVec = glm::vec4(pointVector.x, pointVector.y, pointVector.z, 1);
+		rot = glm::mat4(1.0f);
 		rot = glm::rotate(rot, glm::radians(RotX), glm::vec3(1, 0, 0));
-		rot = glm::rotate(rot, glm::radians(RotY), glm::vec3(0, 1, 0));
-		rot = glm::rotate(rot, glm::radians(RotZ), glm::vec3(0, 0, 1));
-
-		axisY = glm::vec3(tAxisY.x, tAxisY.y, tAxisY.z); // up vector
-		axisZ = glm::vec3(tPoint.x, tPoint.y, tPoint.z); // forward vector
-		axisX = glm::normalize(glm::cross(axisZ, axisY));*/
+		rVec = rVec * rot;
+		tempUp = tempUp * rot;
+		axisY = glm::vec3(tempUp.x, tempUp.y, tempUp.z);
+		pointVector = glm::vec3(rVec.x, rVec.y, rVec.z);
 
 		// Set Camera View (where initalLoc is the camera origin)
 		glm::mat4 View = glm::lookAt(localPos, pointVector + localPos, axisY); // This should act as the new origin
